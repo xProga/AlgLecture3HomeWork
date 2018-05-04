c1be301a7440b42720e4f9f1b6a696f2952ae080
xProga <progavs@gmail.com>
2018-05-04 19:43:28 +0500

    first commit

---
+++ Lecture3HomeWork/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture3HomeWork
+{
+    class Program
+    {
+        static void Swap(ref int elOne, ref int elTwo)
+        {
+            int temp = elOne;
+            elOne = elTwo;
+            elTwo = temp;
+        }
+
+        static void Sort(ref int[] mass)
+        {
+            int masLength = mass.Length;
+            int movCount = 0;
+            for (int i = 0; i < masLength; i++)
+            {
+                movCount = 0;
+                if (i % 2 == 0)
+                {
+                    int j = 0;
+                    while (j < masLength - 1)
+                    {
+                        if (mass[j] > mass[j + 1])
+                        {
+                            Swap(ref mass[j], ref mass[j + 1]);
+                            movCount++;
+                        }
+                        j++;
+                    }
+                }
+                else {
+                    int j = masLength - 1;
+                    while (j > 0)
+                    {
+                        if (mass[j-1] > mass[j])
+                        {
+                            Swap(ref mass[j - 1], ref mass[j]);
+                            movCount++;
+                        }
+                        j--;
+                    }
+                }
+                if (movCount == 0)
+                { break; }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] mass = { 1, 4, 2, 6, 2, 9, 5, 7, 1 };
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.Write($"{mass[i]} ");
+            }
+            Console.WriteLine();
+            Sort(ref mass);
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.Write($"{mass[i]} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lecture3HomeWorkTask5/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture3HomeWorkTask5
+{
+    class Program
+    {
+        static void Input(int i, int pos, ref int[] mas)
+        {
+            if (i > pos)
+            {
+                int keyVal = mas[i];
+                while (i != pos)
+                {
+                    mas[i] = mas[i - 1];
+                    i--;
+                }
+                mas[pos] = keyVal;
+            }
+        }
+
+        static void BinarySearch(ref int[] mas, int keyVal, ref int startPos, ref int endPos, ref int posVal)
+        {
+            while (startPos < endPos)
+            {
+                int mid = startPos + (endPos -startPos) / 2;
+                if (keyVal >= mas[mid])
+                {
+                    startPos = mid + 1;
+                }
+                else 
+                {
+                    endPos = mid;
+                }
+                
+            }
+        }
+
+        static void Sort(ref int[] mas)
+        {
+            for (int i = 0; i < mas.Length; i++)
+            {
+                int pos = -1;
+                int startIndex = 0;
+                int endIndex = i;
+                int keyVal = mas[i];
+                BinarySearch(ref mas, keyVal, ref startIndex, ref endIndex, ref pos);
+                if (keyVal != mas[endIndex])
+                {
+                    Input(i, endIndex, ref mas);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] mass = { 2, 4, 3, 1, 5};
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.Write($"{mass[i]} ");
+            }
+            Console.WriteLine();
+            Sort(ref mass);
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.Write($"{mass[i]} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+ 
